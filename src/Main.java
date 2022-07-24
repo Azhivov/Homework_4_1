9b2f57f6922304f1f614769cf1cd45a29f16fe87
azhiv <a.zhivov@gmail.com>
2022-07-24 18:28:46 +0300

    Задание 4

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,50 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+
     }
     //задание 1
     public static void task1() {
-        int i = 10;
-        int j = 1;
-        while ( j <= 10)
-            System.out.print( j++);
-            System.out.println();
-            for (; i > 0; i = i - 1){
-                System.out.print( i );
+        int counter = 1;
+          while (counter < 11){
+            System.out.print(counter + " ");
+            counter++;
+    }
+        System.out.println();
+            for (int i = counter - 1; i > 0; i --){
+                System.out.print( i + " ");
           }
     }
-    // Задание 2
     public static void task2() {
-        System.out.println("\n");
-        int friyday = 2;
-        for (; friyday <= 31; friyday = friyday + 7){
-           System.out.println("Сегодня пятница " + friyday  + " - число. Необходимо подготовить отчет.");
-                continue;
+        System.out.println();
+        int monthDays = 31;
+        for (int i = 1; i < monthDays; i = i + 7) {
+            System.out.println(i + " - е число. Необходимо подготовить отчет ");
+        }
+        int friday = 1;
+                while (friday<monthDays){
+                    System.out.println(friday + " - е число. Необходимо подготовить отчет " );
+                    friday+=7;
+                }
+    }
+    public static void task3() {
+        int currentYear = 2022;
+        int previousYear = currentYear - 200;
+        int futureYear = currentYear + 100;
+        for (int i = 0; i < futureYear; i = i + 79) {
+            if (i > previousYear) {
+                System.out.println(i);
             }
         }
 
-    }
+        System.out.println(" ");
+        int dayX = 0;
+        while (dayX < futureYear) {
+            if(dayX > previousYear){
+            System.out.println(dayX);
+        }
+            dayX+=79;
+        }
+
+    }}
